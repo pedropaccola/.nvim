b360a262e849b8ca0895c2c2e8f447735c8aaf75
Pedro Paccola <paccolapedro@gmail.com>
2022-12-25 21:39:47 -0300

    nvim-tree glyphs and mappings

--- init.lua
+++ init.lua
@@ -1,3 +1,7 @@
+if vim.g.vscode then
+	return
+end
+
 require("paccola.plugins")
 
 require("paccola.core.options")
--- lua/paccola/plugins/nvim-tree.lua
+++ lua/paccola/plugins/nvim-tree.lua
@@ -3,14 +3,54 @@ if not status_ok then
 	return
 end
 
+local keymappings = {
+	{ key = { "<CR>", "o", "<2-LeftMouse>" }, action = "edit" },
+	{ key = { "O" }, action = "edit_no_picker" },
+	{ key = { "<2-RightMouse>", "<C-]>" }, action = "cd" },
+	{ key = "<C-v>", action = "vsplit" },
+	{ key = "<C-x>", action = "split" },
+	{ key = "<C-t>", action = "tabnew" },
+	{ key = "<", action = "prev_sibling" },
+	{ key = ">", action = "next_sibling" },
+	{ key = "P", action = "parent_node" },
+	{ key = "<BS>", action = "close_node" },
+	{ key = "<Tab>", action = "preview" },
+	{ key = "K", action = "first_sibling" },
+	{ key = "J", action = "last_sibling" },
+	{ key = "I", action = "toggle_ignored" },
+	{ key = "H", action = "toggle_dotfiles" },
+	{ key = "R", action = "refresh" },
+	{ key = "a", action = "create" },
+	{ key = "d", action = "remove" },
+	{ key = "D", action = "trash" },
+	{ key = "r", action = "rename" },
+	{ key = "<C-r>", action = "full_rename" },
+	{ key = "x", action = "cut" },
+	{ key = "c", action = "copy" },
+	{ key = "p", action = "paste" },
+	{ key = "y", action = "copy_name" },
+	{ key = "Y", action = "copy_path" },
+	{ key = "gy", action = "copy_absolute_path" },
+	{ key = "[c", action = "prev_git_item" },
+	{ key = "]c", action = "next_git_item" },
+	{ key = "-", action = "dir_up" },
+	{ key = "s", action = "system_open" },
+	{ key = "q", action = "close" },
+	{ key = "g?", action = "toggle_help" },
+	{ key = "W", action = "collapse_all" },
+	{ key = "S", action = "search_node" },
+}
+
 -- disable netrw (strongly advised)
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 nvim_tree.setup({
-	update_focused_file = {
-		enable = true,
-		update_cwd = true,
+	view = {
+		mappings = {
+			custom_only = true,
+			list = keymappings,
+		},
 	},
 	diagnostics = {
 		enable = true,
@@ -22,11 +62,33 @@ nvim_tree.setup({
 			error = "",
 		},
 	},
+	renderer = {
+		group_empty = true,
+		highlight_git = true,
+		root_folder_modifier = ":~",
+		icons = {
+			glyphs = {
+				git = {
+					unstaged = "",
+					staged = "",
+					unmerged = "",
+					renamed = "➜",
+					untracked = "",
+					deleted = "",
+					ignored = "◌",
+				},
+			},
+		},
+	},
+	update_focused_file = {
+		enable = true,
+		update_cwd = true,
+	},
 	actions = {
 		open_file = {
 			window_picker = {
-				enable = false
-			}
-		}
+				enable = false,
+			},
+		},
 	},
 })
